9e9f1171cbd5db9af4a04ef74aa8604a9d0f03e6
olawale olaleye <waleberry@yahoo.com>
2019-11-09 21:04:58 +0100

    setup Jenkins on Amazon EC2

---
+++ JenkinsEC2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# setup Jenkins on Amazon EC2
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install java-1.8.0 -y
+sudo yum install jenkins -y
+sudo service jenkins start
+#get initial password
+sudo cat
+/var/lib/jenkins/secrets/initialAdminPassword
